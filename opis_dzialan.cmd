de6125bc34dfcdadaf415180e31f91cd9114ae4a
dstronaut <tomaszo24@gmail.com>
2022-12-07 01:49:58 +0100

    final2

--- opis_dzialan.cmd
+++ opis_dzialan.cmd
@@ -1,11 +1,16 @@
-#define ROTATE_SPEED 60
+/*
+* Apollo11 - 20 lipca 1969 (koloryzowane)
+*/
 
-Begin_Parallel_Actions
-Move Rocket 20 10
-Rotate Rocket 50 360
+#define ROTATE_SPEED 50
+#define FLIGHT_SPEED 20
+
+Begin_Parallel_Actions //poszukiwanie miejsca 
+Move Rocket FLIGHT_SPEED 10
+Rotate Rocket ROTATE_SPEED 360
 End_Parallel_Actions
 
-Pause 1000
+Pause 1000 //czekanie na odpowidni moment
 
-Move Rocket 20 10
+Move Rocket FLIGHT_SPEED 10 // lądowanie
 
